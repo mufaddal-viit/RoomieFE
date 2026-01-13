0533b853e9dad5869f9524a886e32731da247de5
mufaddalpam <mufaddal@pamcorpservices.com>
2026-01-13 16:30:44 +0400

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 3001,
@@ -12,10 +11,10 @@ export default defineConfig(({ mode }) => ({
       "/api": "http://localhost:4003",
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
